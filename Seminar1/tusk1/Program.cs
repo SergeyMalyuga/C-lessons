b6a723da8d8742cb02a72d34be4d074563628ce2
Sergey Malyuga <sergey@malyuga.ru>
2023-02-12 13:53:16 +0300

    Added tusk1

---
+++ Seminar1/tusk1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/* 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3, b = 9 -> нет
+*/
+
+Console.WriteLine("Введите число №1!");
+String answer = Console.ReadLine();
+int a = Convert.ToInt32(answer);
+Console.WriteLine("Введите число №2!");
+String answer2 = Console.ReadLine();
+int b = Convert.ToInt32(answer2);
+
+if (a == b * b)
+{
+    Console.WriteLine("a= " + a + " b=" + b + "-> да");
+}
+if (b == a * a)
+{
+    Console.WriteLine("a= " + a + " b=" + b + " -> да");
+}
+else
+{
+    Console.WriteLine("a= " + a + " b=" + b + " -> нет");
+}
