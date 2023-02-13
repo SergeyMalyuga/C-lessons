a373f655f937e4b0ff0de5d2ce01acd261a46749
Sergey Malyuga <sergey@malyuga.ru>
2023-02-13 21:31:09 +0300

    Added Homework tusk4

---
+++ Seminar1/HomeWork/tusk4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+
+
+Console.WriteLine("Введите число!");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+Console.Write(number + " -> ");
+
+for (int i = 2; i <= number; i++)
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+   
+    
