56bbf5d4504ec4bec00d4a951d341d84ce5e1788
Sergey Malyuga <sergey@malyuga.ru>
2023-02-13 21:05:31 +0300

    Added Homework tusk3

---
+++ Seminar1/HomeWork/tusk3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдает, является ли число четным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число!");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine(number + " является чётным числом");
+}
+else
+{
+    Console.WriteLine(number + " не является чётным числом");
+}
