84dd7b6480b7758ef0f41e7828022b1b2b7bba2e
Sergey Malyuga <sergey@malyuga.ru>
2023-02-14 21:33:15 +0300

    Added Seminar2 ClassWork tusk5

---
+++ Seminar2/ClassWork/tusk5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.WriteLine("Введите число №1");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+Console.WriteLine("Введите число №2");
+String answer2 = Console.ReadLine();
+int number2 = Convert.ToInt32(answer2);
+
+if (number2 == number*number)
+{
+    Console.WriteLine(number+" "+number2+" -> да");
+}
+else
+{
+    Console.WriteLine(number+" "+number2+" -> нет");
+}
