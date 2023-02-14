c83aa3f004329b71f42d5b5011dfdca8c4641ada
Sergey Malyuga <sergey@malyuga.ru>
2023-02-14 20:28:19 +0300

    Added Seminar2 ClassWork tusk1

---
+++ Seminar2/ClassWork/tusk1/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 10;
+if (firstDigit > lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(lastDigit);
+}
+// Console.WriteLine($"max {firsDigit}, min {lastDigit}");
