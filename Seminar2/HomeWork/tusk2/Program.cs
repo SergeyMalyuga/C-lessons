69f233491c9339d1c8a580b060da0ac1b5811529
Sergey Malyuga <sergey@malyuga.ru>
2023-02-14 22:52:43 +0300

    Added Seminar2 Homework tusk2

---
+++ Seminar2/HomeWork/tusk2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число!");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+while (number > 1000)
+{
+    number = number / 10;
+}
+
+if (number>=100)
+{
+int thirdNumber = number % 10;
+Console.WriteLine(thirdNumber);
+}
+else
+{
+   Console.WriteLine(number+" -> третьей цифры нет"); 
+}
