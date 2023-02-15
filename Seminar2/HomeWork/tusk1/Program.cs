b011fb782d54bdf2b7a7f9bf9cfc8f85887768c2
Sergey <sergey@malyuga.ru>
2023-02-15 10:20:02 +0300

    Added Seminar2 HomeWork tusk1

---
+++ Seminar2/HomeWork/tusk1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число!");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+if (number >= 100 && number < 1000)
+{
+    int thirdNumber = (number / 10) % 10;
+    Console.WriteLine(number+" -> "+thirdNumber);
+}
+else
+{
+    Console.WriteLine("Вы ввели число, которое не является трёхзначным!");
+}
+
