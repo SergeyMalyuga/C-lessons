044c66701d31e2c01c07d3d09e87a22e687bff5c
Sergey <sergey@malyuga.ru>
2023-02-15 10:34:23 +0300

    Added Seminar2 HomeWork tusk3

---
+++ Seminar2/HomeWork/tusk3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру обозначающую день недели!");
+String answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+if (number >= 1 && number <= 7)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine(number + " ->  да, день является выходным");
+    }
+    else
+    {
+        Console.WriteLine(number + " ->  нет, день не является выходным");
+    }
+}
+else
+{
+    Console.WriteLine("нет такого дня недели!");
+}
