2fe8320f3b364341f5c8f390084ab2b8a83541a0
Sergey Malyuga <sergey@malyuga.ru>
2023-02-17 20:44:55 +0300

    Added Seminar3 ClassWork tusk1

---
+++ Seminar3/ClassWork/tusk1/Program.cs
@@ -0,0 +1,29 @@
+﻿// 
+
+Console.WriteLine("Введите координвту точки по x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координвту точки по y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("точка находится в первой координатной чертвети!");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("точка находится в второй координатной чертвети!");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("точка находится в nhtnmtq координатной чертвети!");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("точка находится в четвёртой координатной чертвети!");
+
+}
+else
+{
+    Console.WriteLine("не возможно определить координатную четверть"); 
+}
