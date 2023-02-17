102fddb7f0fb01671291a8856f468955e1a2c48a
Sergey Malyuga <sergey@malyuga.ru>
2023-02-17 20:58:19 +0300

    Added Seminar3 ClassWork tusk2

---
+++ Seminar3/ClassWork/tusk2/Program.cs
@@ -0,0 +1,28 @@
+﻿// задача №18
+
+Console.WriteLine("Введите номер четверти");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (x==1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+}
+else if (x==2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if (x==3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if (x==4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+
+}
+else
+{
+    Console.WriteLine("не возможно определить координатную четверть"); 
+}
