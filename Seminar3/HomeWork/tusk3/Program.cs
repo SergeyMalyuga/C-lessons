7cc4045e065379abacd3203a6b2e6f4d74f7b994
Sergey <sergey@malyuga.ru>
2023-02-20 15:18:00 +0300

    Added Seminar3 HomeWork tusk3

---
+++ Seminar3/HomeWork/tusk3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №23!
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine ("Введите число!");
+String answer = Console.ReadLine();
+if (double.TryParse(answer, out double number))
+{
+    Console.Write(number+" -> ");
+    for (int i=1; i<=number; i++)
+    {
+    double rez = Math.Pow (i,3);
+    Console.Write(rez+" ");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели не число!");
+}
