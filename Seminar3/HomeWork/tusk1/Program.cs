e9b9a7911ccc102f283300ec412f15502c994dda
Sergey <sergey@malyuga.ru>
2023-02-20 11:19:44 +0300

    Added Seminar3 tusk1

---
+++ Seminar3/HomeWork/tusk1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число!");
+String answer = Console.ReadLine();
+int rez = 0;
+
+if (int.TryParse(answer, out int number)) // проверка ввёл ли пользователь число.
+{
+    int temp = number;
+    if (number > 10000 && number < 100000)
+    {
+        while (temp > 0)
+        {
+            rez = rez * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        if (number == rez)
+        {
+            Console.WriteLine(number + " -> да, число является палиндромом!");
+        }
+        else
+        {
+            Console.WriteLine(number + " -> нет, число не является палиндромом!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не пятизначное число!");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не число!");
+}
