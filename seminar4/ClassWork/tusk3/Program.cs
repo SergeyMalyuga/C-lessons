45f6891cab2527306b81f794e4cc6bd3cd56e53b
Sergey Malyuga <sergey@malyuga.ru>
2023-02-21 21:42:05 +0300

    Added seminar4 ClassWork tusk3

---
+++ seminar4/ClassWork/tusk3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8
+// //элементов, заполненный нулями и единицами в случайном порядке.
+// //[1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+int length = array.Length;
+for (int i = 0; i < length; i++)
+{
+    array[i] = Random.Shared.Next(0, 2);
+}
+foreach (var a in array)
+{
+    Console.Write("{0} ", a);
+}
