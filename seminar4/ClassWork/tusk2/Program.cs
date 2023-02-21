925aa12c9ef9f8480b6730ad1fd7cc6abf76945b
Sergey Malyuga <sergey@malyuga.ru>
2023-02-21 21:06:28 +0300

    Added seminar4 ClassWork tusk2

---
+++ seminar4/ClassWork/tusk2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine ("Введите число!");
+String answer = Console.ReadLine();
+if (int.TryParse(answer, out int number))
+{
+    int count = 0;
+    while (number>0)
+    {
+    number = number/10;
+    count = count+1;
+    }
+    Console.WriteLine(count);
+}
+else 
+{
+    Console.WriteLine ("Вы ввели не число!");
+}
