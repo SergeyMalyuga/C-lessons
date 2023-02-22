5bea644d11e25d8901f5a51c2faea92b286e1eb1
Sergey <sergey@malyuga.ru>
2023-02-22 12:19:52 +0300

    Added seminar4 ClassWork tusk2

---
+++ seminar4/HomeWork/tusk2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber(String messageToUser) // Ввод числа.
+{
+    Console.WriteLine(messageToUser);
+    String answer = Console.ReadLine();
+    int.TryParse(answer, out int num);
+    return num;
+}
+
+int sumNumber(int a) //Сумма цифр в числе.
+{
+    int rez = 0;
+    while (a > 0)
+    {
+        rez = rez + (a % 10);
+        a = a / 10;
+    }
+    return rez;
+}
+
+bool numberCheck(int number) //Проверка больше ли число нуля.
+{
+    if (number > 0)
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("С отрицательными числами не работаю!");
+        return false;
+    }
+}
+
+int number = InputNumber("Введите число!");
+if (numberCheck(number))
+{
+    int sum = sumNumber(number);
+    Console.WriteLine(number + " -> " + sum);
+}
