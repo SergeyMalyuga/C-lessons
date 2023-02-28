aa047aec5bd6246b983d96f83629790828184e8c
Sergey Malyuga <sergey@malyuga.ru>
2023-02-28 22:34:12 +0300

    Seminar6 ClassWork tusk4

---
+++ Seminar6/ClassWork/tusk4/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] randomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(min, max);
+    }
+    return array;
+}
+
+const int LENGTH = 12;
+const int LEFTBORDER = -10;
+const int RIHTBORDER = 10;
+
+int[] array = randomArray(LENGTH, LEFTBORDER, RIHTBORDER);
+int[] array2 = new int [array.Length];
+
+for (int i = 0; i< array2.Length; i++)
+{
+    array2[i] = array[i];
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", array2)}]");
+
