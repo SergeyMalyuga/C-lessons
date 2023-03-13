660da4b76ebcabb62e9cee3706da0b02fd4b3bbd
Andersen <sergey@malyuga.ru>
2023-03-13 21:01:32 +0300

    Added Seminar9 task2,3

---
+++ Seminar9/ClassWork/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 67: 
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+
+
+int x = 475;
+
+
+int SumNumber(int x)
+{
+    
+if (x==0) return 0;
+int rez = x%10+SumNumber(x/10);
+return rez;
+}
+
+int rez = SumNumber(x);
+Console.WriteLine(rez);
